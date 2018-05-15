1acc44d86f8d10045502119c2b3c295405b5dd90
Pavlo_Yemelianov <pavlo_yemelianov@epam.com>
2018-05-15 16:51:19 +0300

    Init commit for Zuul

---
+++ src/main/java/com/epam/javacc/microservices/apigateway/ZuulApplication.java
@@ -0,0 +1,14 @@
+package com.epam.javacc.microservices.apigateway;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ZuulApplication.class).web(true).run(args);
+    }
+}

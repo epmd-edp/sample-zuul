e793897e5c1609befcca4a7121fdaf5c64e68663
super-admin-mr-925-9-edp <1@epam.com>
2018-10-09 14:15:49 +0000

    Insert the description of the change.
    Change-Id: I95370d905b4182967ce205a3803271f32a614d40

--- src/main/java/com/epam/javacc/microservices/apigateway/ZuulApplication.java
+++ src/main/java/com/epam/javacc/microservices/apigateway/ZuulApplication.java
@@ -3,12 +3,14 @@ package com.epam.javacc.microservices.apigateway;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import java.util.Arrays;
 
 @SpringBootApplication
 @EnableZuulProxy
 public class ZuulApplication {
 
     public static void main(String[] args) {
+        Arrays.asList(1, 2).forEach(System.out::println);
         new SpringApplicationBuilder(ZuulApplication.class).web(true).run(args);
     }
 }

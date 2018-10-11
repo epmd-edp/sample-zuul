8dbd59533d349990f0a3d7fb1dd54a679c74f7bb
super-admin-mr-925-15-edp <1@epam.com>
2018-10-11 07:52:01 +0000

    Insert the description of the change.
    Change-Id: I8d3a77cd073580783c15157fbb0690c7cbc54dc3

--- src/main/java/com/epam/javacc/microservices/apigateway/ZuulApplication.java
+++ src/main/java/com/epam/javacc/microservices/apigateway/ZuulApplication.java
@@ -10,6 +10,7 @@ import java.util.Arrays;
 public class ZuulApplication {
 
     public static void main(String[] args) {
+      
         new SpringApplicationBuilder(ZuulApplication.class).web(true).run(args);
     }
 }
